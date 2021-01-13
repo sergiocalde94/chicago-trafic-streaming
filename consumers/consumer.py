447e7bc16a38721e147e0bb679836f53b3b7f195
scalderp <sergiocalde94@gmail.com>
2021-01-13 09:29:10 +0100

    Project Finished
    - Fixed one small error in consumer

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -34,8 +34,8 @@ class KafkaConsumer:
 
         self.broker_properties = {
             'bootstrap.servers': BROKER_URL,
-            "group.id": f'{topic_name_pattern}',
-            'auto.offset.reset': 'earliest'
+            'group.id': f'{topic_name_pattern}',
+            'auto.offset.reset': 'earliest' if offset_earliest else 'latest'
         }
 
         if is_avro is True:
